570d4f3d8be856d826cdbad6f299ecc1fc0ce6b1
eric-zhang1 <ericletianzhang@gmail.com>
2025-02-21 17:45:45 -0500

    Pushing a test file to a test branch

---
+++ Assets/Scripts/Utility/TestScript.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestScript
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Hi, I am testing git
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
